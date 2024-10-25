a7d1dd24a3804c002e6bfef0b58ff23ccbed395f
Shashwat Chaturvedi <shashwatchaturvedi271104@gmail.com>
2024-10-25 16:48:13 +0530

    Authentication usign metamask implemented.

---
+++ src/components/Client/Client.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Client = () => {
+  return (
+    <div>
+      <h2>Client Interface</h2>
+      {/* Add client-specific UI elements here */}
+    </div>
+  );
+};
+
+export default Client;
---
+++ src/components/Freelancer/Freelancer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Freelancer = () => {
+  return (
+    <div>
+      <h2>Freelancer Interface</h2>
+      {/* Add freelancer-specific UI elements here */}
+    </div>
+  );
+};
+
+export default Freelancer;
